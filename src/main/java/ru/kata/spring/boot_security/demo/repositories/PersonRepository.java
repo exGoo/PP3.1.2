94f9e5f06b72fe3f44558c911047ef424f297a7d
Vladislav Guliaev <nascentesxxx@gmail.com>
2024-10-31 22:37:56 +0400

    Fix lazy init

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/PersonRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.Person;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    @Query("select p from Person as p join fetch p.roles")
     Optional<Person> findByUserName(String s);
 }
